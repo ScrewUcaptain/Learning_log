9250eddcfd8e806df508c4b16c930f5c397f7b3e
ScrewUcaptain <basilebrusseau@gmail.com>
2022-06-07 15:10:28 +0200

    starting templates

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Topics
+from .models import Topics, Entry
 
 admin.site.register(Topics)
+admin.site.register(Entry)
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,5 @@
+from statistics import mode
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
@@ -11,3 +13,18 @@ class Topics(models.Model):
     def __str__(self):
         """Return a string representation of the model"""
         return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topics,on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+        
+    def __str__(self):
+        """Return a string representation of the model."""
+        if len(self.text) < 50:
+            return self.text
+        return f"{self.text[:50]}..."
