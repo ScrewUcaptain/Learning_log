67cfe09dc9c2b09c497b0c8e985c25aedfec8a42
ScrewUcaptain <basilebrusseau@gmail.com>
2022-06-08 20:06:47 +0200

    edit entry

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,21 +1,18 @@
-from dataclasses import field, fields
-from importlib.metadata import files
-from pyexpat import model
-from django import forms
-from matplotlib import widgets
 
+from django import forms
 from .models import Topics, Entry
+
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topics
         fields = ['text']
-        labels = {'text':""}
-        
+        labels = {'text': ""}
+
+
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['text']
         labels = {'text': " "}
-        widgets = {"text": forms.Textarea(attrs={'cols':80})}
-        
+        widgets = {"text": forms.Textarea(attrs={'cols': 80})}
